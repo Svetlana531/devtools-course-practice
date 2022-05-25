9eabee052b8a250decc1e07df65eeeccdeb38756
Svetlana531 <60916371+Svetlana531@users.noreply.github.com>
2022-05-25 20:50:31 +0300

    fin

---
+++ modules/shurygina_rev_pol_notat/src/polish_app.cpp
@@ -0,0 +1,31 @@
+// Copyright 2022 Kozel Svetlana
+
+#include <exception>
+#include <iostream>
+#include <string>
+#include <stack>
+#include "include/polish_notation.h"
+#include "include/lexem.h"
+#include "include/stack.h"
+#include "include/rev_pol_app.h"
+
+std::string Application::operator()(int argc, const char* argv[]) {
+  if (argc == 1 || argc > 2) {
+    return this->help(argv[0]);
+  }
+  std::string output {};
+  if(argc == 2) {
+    PolishNotation l1;
+    TQueue<Lexem*>* lex = new TQueue<Lexem*>;
+    std::string str = argv[1];
+    lex = l1.separatExpressionOnLexems(str);
+    output = l1.revPolNot(lex);
+  }
+  return output;
+}
+
+std::string Application::help(const char* appname) {
+  return std::string {"Revers Polish notation\nUsage: "}
+    + std::string {appname}
+    + std::string {" Enter problem <problem>\n        No limits"};
+}
